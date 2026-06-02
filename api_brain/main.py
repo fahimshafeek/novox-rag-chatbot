60c9d374f11d8853294a964d026a918eb696a7a1
fahimshafeek <fahimshafeek@gmail.com>
2026-06-02 11:33:37 +0530

    changes

--- api_brain/main.py
+++ api_brain/main.py
@@ -5,6 +5,9 @@ from fastembed import TextEmbedding
 import requests
 import os
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI(title="Novox EdTech Brain")
 
